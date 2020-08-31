eecde6adc94c837bac092a105495194279505ae9
maalid <maalid@gmail.com>
2020-08-31 16:51:12 -0400

    add guvcview and delete ffmpeg for cam1

--- R/app_server.R
+++ R/app_server.R
@@ -35,32 +35,33 @@ app_server <- function( input, output, session ) {
   ######## shinymanager
   
   observeEvent(input$cam1Settings, {
-    # # # # shell("C: & cd C:/Users/Admin/Downloads/webcam-settings-dialog-windows-master & launchCam1.bat")
-    # webcamSettingsPath <- system.file("app/webcamSettings", package = "snapShooteR")
-    # # # shell(glue::glue("C: & cd {webcamSettingsPath} & launchCam1.bat"))
-    # # camNames <- system(glue::glue('{webcamSettingsPath} & chcp 65001 > nul & ffmpeg -list_devices true -f dshow -i dummy -hide_banner'), intern = TRUE)
-    # # cam1Name <- substr(camNames[[3]], start = 50, stop = nchar(camNames[[3]]) - 1)
-    # # system(glue::glue('{webcamSettingsPath} & ffmpeg -f dshow -show_video_device_dialog true -video_pin_name 1 -i video="{cam1Name}"'), wait = FALSE)
-    # camNames <- system(paste(glue::glue('"{webcamSettingsPath}/ffmpeg.exe"'), 'ffmpeg -list_devices true -f dshow -i dummy -hide_banner'), intern = TRUE)
-    camNames <- system(paste('ffmpeg -list_devices true -f dshow -i dummy -hide_banner'), intern = TRUE)
-    cam1Name <- substr(camNames[[3]], start = 50, stop = nchar(camNames[[3]]) - 1)
-    # system(sprintf("%s %s %s", 
-    #                glue::glue('"{webcamSettingsPath}/ffmpeg.exe"'), 
-    #                'chcp 65001 > nul', 
-    #                glue::glue('ffmpeg -f dshow -show_video_device_dialog true -video_pin_name 2 -i video="{cam1Name}"')),
+    # # # # # shell("C: & cd C:/Users/Admin/Downloads/webcam-settings-dialog-windows-master & launchCam1.bat")
+    # # webcamSettingsPath <- system.file("app/webcamSettings", package = "snapShooteR")
+    # # # # shell(glue::glue("C: & cd {webcamSettingsPath} & launchCam1.bat"))
+    # # # camNames <- system(glue::glue('{webcamSettingsPath} & chcp 65001 > nul & ffmpeg -list_devices true -f dshow -i dummy -hide_banner'), intern = TRUE)
+    # # # cam1Name <- substr(camNames[[3]], start = 50, stop = nchar(camNames[[3]]) - 1)
+    # # # system(glue::glue('{webcamSettingsPath} & ffmpeg -f dshow -show_video_device_dialog true -video_pin_name 1 -i video="{cam1Name}"'), wait = FALSE)
+    # # camNames <- system(paste(glue::glue('"{webcamSettingsPath}/ffmpeg.exe"'), 'ffmpeg -list_devices true -f dshow -i dummy -hide_banner'), intern = TRUE)
+    # camNames <- system(paste('ffmpeg -list_devices true -f dshow -i dummy -hide_banner'), intern = TRUE)
+    # cam1Name <- substr(camNames[[3]], start = 50, stop = nchar(camNames[[3]]) - 1)
+    # # system(sprintf("%s %s %s", 
+    # #                glue::glue('"{webcamSettingsPath}/ffmpeg.exe"'), 
+    # #                'chcp 65001 > nul', 
+    # #                glue::glue('ffmpeg -f dshow -show_video_device_dialog true -video_pin_name 2 -i video="{cam1Name}"')),
+    # #        wait = FALSE,
+    # #        invisible = FALSE,
+    # #        # # intern = TRUE,
+    # #        # ignore.stdout = T,
+    # #        # show.output.on.console = F,
+    # #        minimized = TRUE)
+    # system(paste(glue::glue('ffmpeg -f dshow -show_video_device_dialog true -video_pin_name 2 -i video="{cam1Name}"')),
     #        wait = FALSE,
     #        invisible = FALSE,
     #        # # intern = TRUE,
     #        # ignore.stdout = T,
     #        # show.output.on.console = F,
     #        minimized = TRUE)
-    system(paste(glue::glue('ffmpeg -f dshow -show_video_device_dialog true -video_pin_name 2 -i video="{cam1Name}"')),
-           wait = FALSE,
-           invisible = FALSE,
-           # # intern = TRUE,
-           # ignore.stdout = T,
-           # show.output.on.console = F,
-           minimized = TRUE)
+    system(paste('guvcview --control_only'))
   })
   observeEvent(input$cam2Settings, {
     # # # # shell("C: & cd C:/Users/Admin/Downloads/webcam-settings-dialog-windows-master & launchCam2.bat")
